79181e1a6cb88b353e98df135e05c13430e14816
Nikolay Kostov <admin@nikolay.it>
2014-10-14 18:44:46 +0300

    Updated logging in "App Lifecycle Events demo"

--- 1. Introduction to ASP.NET/ASP.NET-App-Lifecycle-Events/Global.asax.cs
+++ 1. Introduction to ASP.NET/ASP.NET-App-Lifecycle-Events/Global.asax.cs
@@ -1,47 +1,62 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace ASP.NET_App_Lifecycle_Events
 {
     public class Global : System.Web.HttpApplication
     {
+        private static string LogFilePath = Path.GetTempFileName();
+
         protected void Application_Start(object sender, EventArgs e)
         {
-            Trace.WriteLine("Application_Start Called.");
+            Log("Application_Start Called.");
         }
 
         protected void Session_Start(object sender, EventArgs e)
         {
-            Trace.WriteLine("Session_Start Called.");
+            Log("Session_Start Called.");
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            Trace.WriteLine("Application_BeginRequest Called.");
+            Response.Write(string.Format("Log file: {0}", LogFilePath));
+            Log("Application_BeginRequest Called.");
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
-            Trace.WriteLine("Application_AuthenticateRequest Called.");
+            Log("Application_AuthenticateRequest Called.");
         }
 
         protected void Application_Error(object sender, EventArgs e)
         {
-            Trace.WriteLine("Application_Error Called.");
+            Log("Application_Error Called.");
         }
 
         protected void Session_End(object sender, EventArgs e)
         {
-            Trace.WriteLine("Session_End Called.");
+            Log("Session_End Called.");
         }
 
         protected void Application_End(object sender, EventArgs e)
         {
-            Trace.WriteLine("Application_End Called.");
+            Log("Application_End Called.");
         }
+
         protected void Application_EndRequest(object sender, EventArgs e)
         {
-            Trace.WriteLine("Application_EndRequest Called.");
+            Log("Application_EndRequest Called.");
+        }
+
+        private void Log(string line)
+        {
+            if (!File.Exists(LogFilePath))
+            {
+                File.Create(LogFilePath);
+            }
+
+            File.AppendAllLines(LogFilePath, new[] { string.Format("[{0}] {1}", DateTime.Now, line) });
         }
     }
 }
